3d26cac4247961b8aedcdb5e06c53315d8189d9c
Roman <rgvaramadze@bk.ru>
2023-03-10 07:06:38 +0300

    update 0.1

---
+++ main.py
@@ -0,0 +1,14 @@
+# i = input()
+# j = int(input())
+# # if i!=j:
+# #     print((i+j)-1)
+# # else:
+# #     print("Невозможно...")
+
+while True:
+    i = input()
+    j = input()
+    if i.isdigit() == False or j.isdigit() == False:
+        print(1)
+    else:
+        print(23)
